42e4accad97a5683812247ca5e5e75bc35c8426c
TJesT <alexeynospid@gmail.com>
2024-03-26 17:11:20 +0700

    table Teacher + migrations

--- alembic/env.py
+++ alembic/env.py
@@ -5,17 +5,19 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+import os
+
 from alembic import context
 
-from dotenv import load_dotenv
 from src.core.db import PostgresBase
+from src.core.config import PostgresSettings
 from src.models import *
 
-load_dotenv(".env")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", PostgresSettings.uri)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
